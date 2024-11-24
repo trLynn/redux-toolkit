34b3ec3d823ce5a39c027d4f59edfe88e80b562e
trLynn <thureinlynn.acc4889@gmail.com>
2024-11-24 15:06:04 +0700

    react auth

---
+++ 08_lesson/src/app/api/apiSlice.js
@@ -0,0 +1,39 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { setCredentials, logOut } from "../../features/auth/authSlice";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost: 3500",
+  credentials: "include",
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token;
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithReauth = async (args, api, extraOptins) => {
+  let result = await baseQuery(args, api, extraOptins);
+
+  if (result?.error?.originalStatus === 403) {
+    console.log("sending refresh token");
+    // send refresh token to get new access token
+    const refreshResult = await baseQuery("/refresh", api, extraOptins);
+    if (refreshResult?.data) {
+      const user = api.getState().auth.user;
+      // store the new token
+      api.dispatch(setCredentials({ ...refreshResult.data, user }));
+      // retry the original query with new access token
+      result = await baseQuery(args, api, extraOptins);
+    } else {
+      api.dispatch(logOut());
+    }
+  }
+  return result;
+};
+
+export const apiSlice = createApi({
+    baseQuery: baseQueryWithReauth,
+    endpoints: builder => ({})
+})
---
+++ 08_lesson/src/features/auth/Login.js
@@ -0,0 +1,87 @@
+import { useNavigate } from "react-router-dom";
+
+import { useDispatch } from "react-redux";
+import { setCredentials } from "./authSlice";
+import { useLoginMutation } from "./authApiSlice";
+import { useEffect, useRef, useState } from "react";
+
+const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const userData = await Login({ user, pwd }).unwrap();
+      dispatch(setCredentials({ ...userData, user }));
+      setUser("");
+      setPwd("");
+      navigate("/welcome");
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Missing Username or Password");
+      } else if (err.response?.status === 401) {
+        setErrMsg("Unauthorized");
+      }
+      errRef.current.focus();
+    }
+  };
+
+  const handleUserInput = (e) => setUser(e.target.value);
+
+  const handlePwdInput = (e) => setPwd(e.target.value);
+
+  const content = isLoading ? (
+    <h1>Loading...</h1>
+  ) : (
+    <section className="login">
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <h1>Employee Login</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          value={user}
+          onChange={handleUserInput}
+          autoComplete="off"
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={handlePwdInput}
+          value={pwd}
+          required
+        />
+        <button>Sign In</button>
+      </form>
+    </section>
+  );
+
+  return content;
+};
+
+export default Login;
